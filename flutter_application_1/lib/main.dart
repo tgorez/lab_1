f6846d5813f8678e1ece93428e85cea173098338
Timur Abuov <Bisb22-19@NTDOM.LOCAL>
2026-01-20 09:40:51 +0500

    lab_1

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,35 @@
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Text("Hello"),
+//     );
+//   }  
+// }
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(appBar: AppBar(backgroundColor: Colors.pink,),
+      body: Container(color: Colors.purple, child: Text("Mobile Programming")),)
+    );
+  }
+}
